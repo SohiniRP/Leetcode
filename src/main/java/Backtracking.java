612e372629f8c98c21bfceabda0aa679cbbebe08
Sohini47 <paul47sohini@gmail.com>
2026-04-01 19:04:54 +0530

    31/3 commit

--- src/main/java/Backtracking.java
+++ src/main/java/Backtracking.java
@@ -41,4 +41,29 @@ public class Backtracking {
             current.remove(current.size() - 1); // backtrack
         }
     }
+
+    //797. All Paths From Source to Target
+    public static List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+
+        current.add(0); // start from source
+        backtrackAllPath(graph, 0, current, result);
+
+        return result;
+    }
+
+    public static void backtrackAllPath(int[][] graph, int node, List<Integer> current, List<List<Integer>> result) {
+
+        if (node == graph.length - 1) {
+            result.add(new ArrayList<>(current)); // copy
+            return;
+        }
+
+        for (int next : graph[node]) {
+            current.add(next);                 // choose
+            backtrackAllPath(graph, next, current, result); // explore
+            current.remove(current.size() - 1); // undo (backtrack)
+        }
+    }
 }
